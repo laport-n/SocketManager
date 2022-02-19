58562a1ccb49ee69df3fe4c8b0f2fcfabef46e80
laport-n <nicolas-1301@live.fr>
2022-02-19 16:22:10 +0100

    fix: architecure repo

--- src/modules/redis/config.ts
+++ src/modules/redis/config.ts
@@ -1,11 +1,11 @@
 import { RedisClientOptions, RedisScripts } from "@node-redis/client";
 
 export const config:  Omit<RedisClientOptions<never, RedisScripts>, "modules"> = {
-    url: process.env.SOCKET_REDIS_URL ? process.env.SOCKET_REDIS_URL : 'redis://127.0.0.1:6379',
+    url: process.env.SOCKET_REDIS_URL || 'redis://127.0.0.1:6379',
     socket: undefined, // RedisSocketOptions,
-    username: process.env.SOCKET_REDIS_USERNAME ? process.env.SOCKET_REDIS_USERNAME : undefined,
-    password: process.env.SOCKET_REDIS_PASSWORD ? process.env.SOCKET_REDIS_PASSWORD : 'toto',
-    name: process.env.SOCKET_REDIS_NAME ? process.env.SOCKET_REDIS_NAME : undefined,
+    username: process.env.SOCKET_REDIS_USERNAME || undefined,
+    password: process.env.SOCKET_REDIS_PASSWORD|| 'toto',
+    name: process.env.SOCKET_REDIS_NAME || undefined,
     database: process.env.SOCKET_REDIS_DATABASE ? parseInt(process.env.SOCKET_REDIS_DATABASE) : undefined,
     commandsQueueMaxLength: process.env.SOCKET_REDIS_COMMANDS_QUEUE_MAX_LENGTH ? parseInt(process.env.SOCKET_REDIS_COMMANDS_QUEUE_MAX_LENGTH) : undefined,
     readonly: false,
